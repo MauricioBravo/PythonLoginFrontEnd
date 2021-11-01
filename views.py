dc51299c7d5c15209236cdbf70e65a4bbe551b7f
MauricioBravo <qwe455r@gmail.com>
2021-10-31 22:22:41 -0300

    first commit 1

---
+++ views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+import datetime
+
+def login(request):  #ESTO ES UNA VISTA
+
+    pagina="<html><head><title>Welcomee</title>  </head><body><div class='welcome'><center><h1><font size='13' color='black'>Welcome</font></h1></center></div><center><hr></center><center><a href='/logout'>Logout</a></center><div class='breaker'><br /><br /><br /><br /><br /><br /><br /></div><div class='footer'><footer style='position: fixed; left: 2; right: 2; width: 95%; bottom: 0;'><center>This webpage is only accessible behind a User Web Login System written in Python</center></footer></div></body></html>"
+    
+    return HttpResponse(pagina)
+
+def damefecha(request):
+
+    fecha_actual=datetime.datetime.now()
+
+    fech="""<html><body><h1> La fecha y hora es   %s </h1></body></html>""" % fecha_actual
+
+    return HttpResponse(fech)
