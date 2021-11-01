90cc0783153617dfb3ea84ff7ef953f0af9386dc
MauricioBravo <qwe455r@gmail.com>
2021-10-31 23:20:22 -0300

    con login en plantilla separada

--- views.py
+++ views.py
@@ -1,18 +1,27 @@
 from django.http import HttpResponse
 import datetime
+from django.template import Template, Context
 
 def login(request):  #ESTO ES UNA VISTA
+    #Cargar la direccion de la plantilla de html en la variable plantilla_externa
+    plantilla_externa=open("C:/Users/Silencer/Documents/UNIVERSIDAD/4TO AÑO/SEGUNDO SEMESTRE/Arq Palma/python/pythonFrontEnd/Plantillas/PlantillaLogin.html")
 
-    pagina="<html><head><title>Welcomee</title>  </head><body><div class='welcome'><center><h1><font size='13' color='black'>Welcome</font></h1></center></div><center><hr></center><center><a href='/logout'>Logout</a></center><div class='breaker'><br /><br /><br /><br /><br /><br /><br /></div><div class='footer'><footer style='position: fixed; left: 2; right: 2; width: 95%; bottom: 0;'><center>This webpage is only accessible behind a User Web Login System written in Python</center></footer></div></body></html>"
-    
+    plt=Template(plantilla_externa.read())
+
+    plantilla_externa.close() #se cierra luego de consumierlo para ahorrar recursos
+
+    ctx=Context()
+    #la variable pagina consume el documento abierto en plt para renderizarlo con el metodo render y el contexto que en este caso es vacio
+    pagina=plt.render(ctx)
+
     return HttpResponse(pagina)
 
 def damefecha(request):
 
     fecha_actual=datetime.datetime.now()
 
-    fech="""<html><body><h1> La fecha y hora es   %s </h1></body></html>""" % fecha_actual
+    fech="""<html><body><h1> La fecha y hora es ñ  %s </h1></body></html>""" % fecha_actual
 
     return HttpResponse(fech)
-    
+
     variable=null
