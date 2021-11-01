c9c20e0f30c4f0c94f329b5facaaeb6f9ad58f65
MauricioBravo <qwe455r@gmail.com>
2021-11-01 11:05:39 -0300

    con otp y seguridad, funciona, revisar auth de google

--- views.py
+++ views.py
@@ -1,20 +1,50 @@
 from django.http import HttpResponse
 import datetime
+import pyotp
+import qrcode
 from django.template import Template, Context
 
 def login(request):  #ESTO ES UNA VISTA
+   
+    
+ #codigo de nico (funciona)
+    clave = pyotp.random_base32()
+    #print("Clave:", clave)
+    totp_object = pyotp.TOTP(clave)
+    #Se realiza la Autenticacion por medio de otp utilizando el algoritmo de totp
+    qr_text = totp_object.provisioning_uri(name="2FA", issuer_name="Seguridad")
+    #print(qr_text)
+    #Genera un QR
+    img = qrcode.make(qr_text)
+    f = open("output.png", "wb")
+    img.save(f)
+    f.close()
+
+    #Validar con el Google Authenticator ingresando el codigo que te genera
+    otp = input("ingresar el OTP: ")
+    validar = totp_object.verify(otp)
+    #print(validar)
+    
+
+
+
     #Cargar la direccion de la plantilla de html en la variable plantilla_externa
     plantilla_externa=open("C:/Users/Silencer/Documents/UNIVERSIDAD/4TO AÑO/SEGUNDO SEMESTRE/Arq Palma/python/pythonFrontEnd/Plantillas/PlantillaLogin.html")
-
     plt=Template(plantilla_externa.read())
-
+    nombre="Juan"
     plantilla_externa.close() #se cierra luego de consumierlo para ahorrar recursos
 
-    ctx=Context()
-    #la variable pagina consume el documento abierto en plt para renderizarlo con el metodo render y el contexto que en este caso es vacio
+    ctx=Context({"nombre_persona":nombre}) #context puede recibir diccionarios como parametros, un diccionario tiene una clave y un valor
+    #la variable pagina consume el documento abierto en plt para renderizarlo con el metodo render y el contexto como argumento de render 
     pagina=plt.render(ctx)
 
     return HttpResponse(pagina)
+
+
+
+
+
+
 
 def damefecha(request):
 
@@ -24,4 +54,4 @@ def damefecha(request):
 
     return HttpResponse(fech)
 
-    variable=null
+  
